3bfac42065bec97e36feff72252185aa4ca51bc5
Michael <michael.dlrosa@arcxsolutions.com>
2026-03-16 15:25:56 +0800

    fix:

--- oceansan-backend/src/routes/license.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { activateLicense, getLicenseStatus } from "../controllers/license.controller";
-
-const router = Router();
-
-router.get("/", getLicenseStatus);
-router.post("/activate", activateLicense);
-
-export default router;
